d07a4b30df18e4e7a37570d523117e9717b1c214
josh sorensen <jxsoren@gmail.com>
2021-01-27 20:58:40 -0700

    second commit

--- src/Generator.js
+++ src/Generator.js
@@ -2,16 +2,24 @@ import React from 'react'
 import Form from "./Form"
 function Generator(props) {
         return (
-            <div id="genParent">
-                <h1>React Meme Generator !</h1>
-                <Form />
-                <div className="memeFormat">
-                    <span className="imageBox">
-                        <img src={props.imgUrl} alt="meme"/>
-                    </span>
-                    <h3>Meme Name: {props.name}</h3>
-                    <button onClick={props.handleClick}>New Meme</button>
+            <div>
+                <div id="genParent">
+                    <h1>React Meme Generator!</h1>
+                    <Form />
+                    <div className="memeFormat">
+                        <span className="imageBox">
+                            <img src={props.imgUrl} alt="meme"/>
+                        </span>
+                        <h3>Meme Name: {props.name}</h3>
+                        <button onClick={props.handleClick}>New Meme</button>
+                    </div>
                 </div>
+
+                <div className="userCreatedMemes">
+                    <hr />
+                    <h1>Created Memes</h1>
+                </div>
+
             </div>
         )
 }
