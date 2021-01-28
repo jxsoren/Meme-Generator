e3955a4c2202fcbbb1a964d9289903c36d602771
Garrett England <garrett.t.england@gmail.com>
2021-01-28 16:43:03 -0700

    Live Preview feature added

--- src/Generator.js
+++ src/Generator.js
@@ -5,13 +5,17 @@ function Generator(props) {
             <div>
                 <div id="genParent">
                     <h1>React Meme Generator!</h1>
-                    <Form />
+                    <Form state={props.state} onChange={props.onChange}/>
                     <div className="memeFormat">
-                        <span className="imageBox">
-                            <img src={props.imgUrl} alt="meme"/>
-                        </span>
-                        <h3>Meme Name: {props.name}</h3>
-                        <button onClick={props.handleClick}>New Meme</button>
+                        <form>
+                            <div className="imageBox">
+                                <img src={props.imgUrl} alt="meme"/>
+                                <h2 className="top">{props.state.topText}</h2>
+                                <h2 className="bottom">{props.state.bottomText}</h2>
+                            </div>
+                            <h3>Meme Name: {props.name}</h3>
+                            <button onClick={props.handleClick}>New Meme</button>
+                        </form>
                     </div>
                 </div>
 
