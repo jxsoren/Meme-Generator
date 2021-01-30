ae971c946f9530b972b5c709d2d6ec76adcf5029
Garrett England <garrett.t.england@gmail.com>
2021-01-29 17:01:58 -0700

    All assignment features included

--- src/CreatedMemes.js
+++ src/CreatedMemes.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import EditMeme from "./EditMeme"
 
 function CreatedMemes(props){
     return(
@@ -9,6 +10,12 @@ function CreatedMemes(props){
                     <img src={props.url} alt="meme"/>
                     <p className="bottomText">{props.bottomText}</p>
                 </span>
+                <button className="deleteButton" onClick={() => props.handleDelete(props.id)}>Delete</button>
+                <button className="editButton" onClick={() => props.handleEdit(props.id)}>Edit</button>
+                <div>
+                    {props.item.isEditing === true ? <EditMeme id={props.item.uniqueId} handleEditChange={props.handleEditChange} handleSave={props.handleSave}/> : null}
+                </div>
+                
             </div>
             <hr />
         </div>
